8789811b75198631f777a7dc0386bf6044aa211c
vcoron <93163519+vcoron@users.noreply.github.com>
2021-11-08 13:35:19 +0300

    Add files via upload

---
+++ js/plugins.js
@@ -0,0 +1,46 @@
+// JavaScript Document
+/*global $, document, window*/
+
+$(document).ready(function (){
+
+	
+
+	$('.carousel').carousel({
+		interval:2000
+		
+		
+	});
+
+	var scrollButton = $("#scroll-top");
+	var navbar1 = $(".navbar");
+	$(window).scroll(function ()
+	{
+		
+		if($(this).scrollTop()>=700)
+			{
+				scrollButton.show();
+			
+			}else{
+				scrollButton.hide();
+			}
+			});
+			$(window).scroll(function ()
+			{
+					
+			if($(this).scrollTop()>=300)
+			{
+				navbar1.addClass('fixed-top');
+			}else{
+				navbar1.removeClass('fixed-top');
+			}
+			});
+	scrollButton.click(function (){
+			$("html,body").animate({ scrollTop : 0 }, 600 );
+		});
+
+
+	
+	
+
+});
+
